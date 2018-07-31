938846a35207e880e1ce89da789f28d109ca535a
unknown <davidserranogemes@hotmail.com>
2018-08-01 01:06:48 +0200

    Added observers and MLPC disabled

--- predator.py
+++ predator.py
@@ -7,3 +7,27 @@ This file contains the object predator
 @author: david
 """
 
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+
+class PredatorAI:
+    brain = 0
+
+    #def __init__(self, board_size, num_layer,l_rate,function, n_iter):
+     #   self.brain = MLPClassifier(hidden_layer_sizes=(board_size,num_layer),learning_rate='adaptative',learning_rate_init=l_rate, activation=function, max_iter=n_iter )
+
+class Predator(object):
+    _x_pos = 0
+    _y_pos = 0
+    
+    def get_X(self):
+        return self._x_pos
+    
+    def get_Y(self):
+        return self._y_pos
+    
+    def set_X(self,x):
+        self._x_pos = x 
+    
+    def set_Y(self,Y):
+        self._y_pos = Y
--- prey.py
+++ prey.py
@@ -13,10 +13,22 @@ import numpy as np
 class PreyAI:
     brain = 0
 
-    def __init__(self, board_size, num_layer,l_rate,function, n_iter):
-        self.brain = MLPClassifier(hidden_layer_sizes=(board_size,num_layer),learning_rate='adaptative',learning_rate_init=l_rate, activation=function, max_iter=n_iter )
+   # def __init__(self, board_size, num_layer,l_rate,function, n_iter):
+    #    self.brain = MLPClassifier(hidden_layer_sizes=(board_size,num_layer),learning_rate='adaptative',learning_rate_init=l_rate, activation=function, max_iter=n_iter )
 
 class Prey(object):
-    x_pos = 0
-    y_pos = 0
+    _x_pos = 0
+    _y_pos = 0
     
+    
+    def get_X(self):
+        return self._x_pos
+    
+    def get_Y(self):
+        return self._y_pos
+    
+    def set_X(self,x):
+        self._x_pos = x 
+    
+    def set_Y(self,Y):
+        self._y_pos = Y
