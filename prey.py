3da309d378eab2e30c39913782e553f42989c868
unknown <davidserranogemes@hotmail.com>
2018-08-07 14:35:06 +0200

    Working in the log system to the prey

--- prey.py
+++ prey.py
@@ -9,6 +9,7 @@ This file contains the object prey
 
 from sklearn.neural_network import MLPClassifier
 import numpy as np
+import pandas as pd
 
 class PreyAI:
     brain = 0
@@ -22,16 +23,39 @@ class Prey(object):
     
     _smell_range = 3
     
+    _data_register_list = 0
+    
     def __init__(self,num_epochs,num_turns,smell_range = 3):
         self._x_pos = np.ones((num_epochs,num_turns))
         self._y_pos = np.ones((num_epochs,num_turns))
         self._smell_range = smell_range
-    
+        
+        self._data_register_list = list()
+        self._data_register_list.insert(pd.DataFrame(np.random.randint(low=0, high=10, size=(0, 16)), columns = ['Predator UP','Predator RIGHT','Predator DOWN','Predator RIGHT','Limit UP','Limit RIGHT','Limit DOWN','Limit LEFT','Go UP','GO UP-RIGHT','GO RIGHT','GO DOWN-RIGHT','GO DOWN','GO DOWN-LEFT','GO LEFT','GO UP-LEFT']))
+        
+        
     def get_X(self,epoch=0,turn=0):
         return int(self._x_pos[epoch,turn])
     
     def get_Y(self,epoch=0,turn=0):
         return int(self._y_pos[epoch,turn])
+    
+    def get_smell_range(self):
+        return self._smell_range
+    def add_register(self, register,epoch):
+        if len(self._data_register_list) >= epoch:
+            #Add the register normally
+            self._data_register_list[epoch].loc[len(self._data_register_list[epoch])] = register
+        else:
+            if len(self._data_register_list) == epoch+1:
+                #Create new  dataframe to the list and add the first register
+                self._data_register_list.append(pd.DataFrame(np.random.randint(low=0, high=10, size=(0, 16)), columns = ['Predator UP','Predator RIGHT','Predator DOWN','Predator RIGHT','Limit UP','Limit RIGHT','Limit DOWN','Limit LEFT','Go UP','GO UP-RIGHT','GO RIGHT','GO DOWN-RIGHT','GO DOWN','GO DOWN-LEFT','GO LEFT','GO UP-LEFT']))
+                self._data_register_list[epoch].loc[len(self._data_register_list[epoch])] = register
+            else:
+                print("Selected epoch is too big")
+    
+                
+    
     
     def set_X(self,x,epoch=0,turn=0):
         self._x_pos[epoch,turn] = x 
@@ -39,8 +63,6 @@ class Prey(object):
     def set_Y(self,Y,epoch=0,turn=0):
         self._y_pos[epoch,turn] = Y
         
-    def get_smell_range(self):
-        return self._smell_range
-    
     def set_smell_range(self,range):
         self._smell_range = range
+        
